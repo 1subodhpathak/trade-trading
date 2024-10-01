2a8585d3e45d185ce2018e8c356e7002d92776cb
1subodhpathak <subodhpathak1511@gmail.com>
2024-10-01 23:39:39 +0530

    Day-0

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,22 @@
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
+import Dashboard from './pages/dashboard/Dashboard'
 import HeaderView from './pages/home/HeaderView'
 
 function App() {
-  const [message, setMessage] = useState();
+  // const [message, setMessage] = useState();
 
-  useEffect(() => {
-    fetch('http://localhost:3000/')
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message))
-      .catch((error) => console.error('Error fetching data:', error));
-  }, []);
+  // useEffect(() => {
+  //   fetch('http://localhost:3000/')
+  //     .then((res) => res.json())
+  //     .then((data) => setMessage(data.message))
+  //     .catch((error) => console.error('Error fetching data:', error));
+  // }, []);
 
   return (
-    <main className='max-w-7xl mx-auto'>
-      <h1>{message}</h1>
+    <main className='max-w-8xl mx-auto'>
+      {/* <h1>{message}</h1> */}
      <HeaderView/>
+     <Dashboard/>
     </main>
   )
 }
